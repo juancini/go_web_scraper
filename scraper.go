9dcbda65f03090682656fe906d66e4c7216c0364
juancini <juani.mancini98@gmail.com>
2024-04-06 18:36:02 -0300

    First scrap done, added demostration csv

--- scraper.go
+++ scraper.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
 
 	// Colly is an open source WEB scrapping library
 	"github.com/gocolly/colly"
 )
 
+type PokemonProduct struct {
+	url, image, name, price string
+}
+
 func main() {
-	// scrapping logic
+	// initializing the slice of structs that will contain the scraped data
+	var pokemonProducts []PokemonProduct
+
+	// input args
 	args := os.Args
 	url := args[1]
+	// new colly collector
 	c := colly.NewCollector()
-	c.Visit("https://scrapeme.live/shop/")
 
+	// scrapping logic
 	// whenever the collector is about to make a new request
 	c.OnRequest(func(r *colly.Request) {
 		// print the url of that request
@@ -23,10 +33,58 @@ func main() {
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Got a response from", r.Request.URL)
 	})
+
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		// initializing a new PokemonProduct instance
+		pokemonProduct := PokemonProduct{}
+		// scraping the data of interest
+		pokemonProduct.url = e.ChildAttr("a", "href")
+		pokemonProduct.image = e.ChildAttr("img", "src")
+		pokemonProduct.name = e.ChildText("h2")
+		pokemonProduct.price = e.ChildText(".price")
+
+		// adding the product instance with scraped data to the list of products
+		pokemonProducts = append(pokemonProducts, pokemonProduct)
+	})
+
 	c.OnError(func(r *colly.Response, e error) {
 		fmt.Println("Blimey, an error occurred!:", e)
 	})
 	c.Visit(url)
+
+	// opening the CSV file
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatalln("Failed to create output CSV file", err)
+	}
+	defer file.Close()
+
+	// initializing a file writer
+	writer := csv.NewWriter(file)
+
+	// writing the CSV headers
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+	writer.Write(headers)
+
+	// writing each Pokemon product as a CSV row
+	for _, pokemonProduct := range pokemonProducts {
+		// converting a PokemonProduct to an array of strings
+		record := []string{
+			pokemonProduct.url,
+			pokemonProduct.image,
+			pokemonProduct.name,
+			pokemonProduct.price,
+		}
+
+		// adding a CSV record to the output file
+		writer.Write(record)
+	}
+	defer writer.Flush()
 
 	fmt.Println("Finish!")
 }
